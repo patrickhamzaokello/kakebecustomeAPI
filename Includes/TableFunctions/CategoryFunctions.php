95468be9955aa4f6802ccb4ca5eecea9a46a2d3d
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2022-02-11 12:21:31 -0800

    prevous searches fetcing

--- Includes/TableFunctions/CategoryFunctions.php
+++ Includes/TableFunctions/CategoryFunctions.php
@@ -74,6 +74,109 @@ class CategoryFunctions
 		return $itemRecords;
 	}
 
+
+	function searchHomePage(){
+		$this->pageno = floatval($this->page);
+		$no_of_records_per_page = 10;
+		$offset = ($this->pageno - 1) * $no_of_records_per_page;
+
+		$sql = "SELECT COUNT(DISTINCT(category_id)) as count FROM products WHERE published = 1 ORDER BY `products`.`featured` DESC limit 1";
+		$result = mysqli_query($this->conn, $sql);
+		$data = mysqli_fetch_assoc($result);
+		$total_rows = floatval($data['count']);
+		$total_pages = ceil($total_rows / $no_of_records_per_page);
+
+
+
+		$categoryids = array();
+		$menuCategory = array();
+		$itemRecords = array();
+
+
+
+		if($this->page == 1 ){
+			// Todays Deal Begin
+
+			$bestsellingProductsID = array();
+			$bestSellingProducts = array();
+			$category_stmts = "SELECT `id`, `query`, `count`, `created_at`, `updated_at` FROM `searches` ORDER BY count DESC LIMIT 10";
+			$menu_type_id_results = mysqli_query($this->conn, $category_stmts);
+
+			while ($row = mysqli_fetch_array($menu_type_id_results)) {
+
+				array_push($bestsellingProductsID, $row);
+			}
+
+			foreach ($bestsellingProductsID as $row) {
+				$product = new Product($this->conn, intval($row['id']));
+				$temp = array();
+				$temp['id'] = $row['id'];
+				$temp['query'] = $row['query'];
+				$temp['count'] = $row['count'];
+				$temp['created_at'] = $row['created_at'];
+				$temp['updated_at'] = $row['updated_at'];
+		
+
+				array_push($bestSellingProducts, $temp);
+			}
+
+
+			$best_temps = array();
+			$best_temps['name'] =  "Searches";		
+			$best_temps['tag'] =  "Whats Trending";	
+			$best_temps['searches'] = $bestSellingProducts;
+
+			$slider_temps = array();
+			$slider_temps['previousSearches'] = $best_temps;
+			array_push($menuCategory, $slider_temps);
+
+			// end Todays Deal  Fetch
+		}
+
+
+		//fetch other categories Begin
+
+		$category_stmt = "SELECT DISTINCT(category_id) FROM products  WHERE published = 1 ORDER BY `products`.`featured` DESC LIMIT " . $offset . "," . $no_of_records_per_page . "";
+		$menu_type_id_result = mysqli_query($this->conn, $category_stmt);
+
+		while ($row = mysqli_fetch_array($menu_type_id_result)) {
+
+			array_push($categoryids, $row);
+		}
+
+		foreach ($categoryids as $row) {
+			$category = new Category($this->conn, intval($row['category_id']));
+			$temp = array();
+			$temp['id'] = $category->getId();
+			$temp['parent_id'] = $category->getParent_id();
+			$temp['level'] = $category->getLevel();
+			$temp['name'] =  $category->getName();
+			$temp['order_level'] =  $category->getOrder_level();
+			$temp['commision_rate'] = $category->getCommission_rate();
+			$temp['banner'] = $category->getBanner();
+			$temp['icon'] = $category->getIcon();
+			$temp['featured'] = $category->getFeatured();
+			$temp['top'] = $category->getTop();
+			$temp['digital'] = $category->getDigital();
+			$temp['slug'] =  $category->getSlug();
+			$temp['meta_title'] = $category->getMeta_title();
+			$temp['meta_description'] = $category->getMeta_description();
+			$temp['created_at'] = $category->getCreated_at();
+			$temp['updated_at'] = $category->getUpdated_at();
+			$temp['products'] = $category->getCategoryProducts();
+			array_push($menuCategory, $temp);
+		}
+		
+
+
+		$itemRecords["page"] = $this->pageno;
+		$itemRecords["categories"] = $menuCategory;
+		$itemRecords["total_pages"] = $total_pages;
+		$itemRecords["total_results"] = $total_rows;
+
+		return $itemRecords;
+	}
+
 	function allCombined()
 	{
 
