c4e2bbae175fbc536b69bbf4a627ff7cce0ea577
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2022-02-23 11:38:10 +0300

    using parent_id working

--- Includes/TableFunctions/Prada.php
+++ Includes/TableFunctions/Prada.php
@@ -1,6 +1,6 @@
 <?php
 
-class ProductDetails
+class Prada
 {
 
     private $ImageBasepath = "https://zodongofoods.com/admin/pages/";
@@ -30,30 +30,10 @@ class ProductDetails
             $no_of_records_per_page = 10;
             $offset = ($this->pageno - 1) * $no_of_records_per_page;
 
-            //get category ids with the same parent id
-            $sibling_cat = array();
-            $sibling_cat_sql = "SELECT id FROM `categories` WHERE parent_id = " . $this->input_menu_type_id . " ";
-            $sibling_cat_sql_result = mysqli_query($this->conn, $sibling_cat_sql);
-            while ($row = mysqli_fetch_array($sibling_cat_sql_result)) {
-                array_push($sibling_cat, $row['id']);
-            }
-
-            $prod_string = "SELECT `id` From products WHERE published = 1 AND (";
-            foreach ($sibling_cat as $cat_id) {
-                $prod_string .= "category_id = " . $cat_id . " OR ";
-            }
-
-            //remove the last OR
-            $prod_string = substr($prod_string, 0, strlen($prod_string) - 4);
-            // add the last bracket
-            $prod_string .=")"; 
-
-            // run the query in the db and search through each of the category returned
-            $prod_id = mysqli_query($this->conn, $prod_string);
-            $result_count = mysqli_num_rows($prod_id);
-
-
-            $total_rows = floatval(number_format($result_count));
+            $sql = "SELECT COUNT(*) as count FROM products WHERE category_id = " . $this->input_menu_type_id . " limit 1";
+            $result = mysqli_query($this->conn, $sql);
+            $data = mysqli_fetch_assoc($result);
+            $total_rows = floatval($data['count']);
             $total_pages = ceil($total_rows / $no_of_records_per_page);
 
 
@@ -65,14 +45,11 @@ class ProductDetails
 
             // get products id from the same cat
             $same_cat_IDs = array();
-            $same_cat_stm = $prod_string . " ORDER BY `products`.`created_at` DESC LIMIT " . $offset . "," . $no_of_records_per_page;
+            $same_cat_stm = "SELECT `id` From products WHERE category_id = " . $this->input_menu_type_id . " ORDER BY num_of_sale DESC LIMIT " . $offset . "," . $no_of_records_per_page;
             $same_cat_id_result = mysqli_query($this->conn, $same_cat_stm);
             while ($row = mysqli_fetch_array($same_cat_id_result)) {
                 array_push($same_cat_IDs, $row['id']);
             }
-
-            // echo $same_cat_stm;
-
 
 
             if ($this->pageno == 1) {
@@ -87,7 +64,7 @@ class ProductDetails
                 $sel_category = array();
 
 
-                if ($menu_type_data) {
+                if($menu_type_data){
                     $menu_type_temp = array();
                     $menu_type_temp['name'] = $cat_name;
                     $menu_type_temp['meta_description'] = $cat_metadescription;
@@ -108,7 +85,7 @@ class ProductDetails
 
 
             foreach ($same_cat_IDs as $row) {
-                $product = new Product($this->conn, $row);
+                $product = new Product($this->conn,$row);
                 $temp = array();
                 $temp['id'] = $product->getId();
                 $temp['name'] = $product->getName();
@@ -126,6 +103,9 @@ class ProductDetails
 
                 array_push($itemRecords["selected_category"], $temp);
             }
+
+
+
         }
 
         return $itemRecords;
@@ -297,4 +277,5 @@ class ProductDetails
 
         return $itemRecords;
     }
+
 }
