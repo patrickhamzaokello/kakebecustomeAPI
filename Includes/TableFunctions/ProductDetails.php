a21c2f8ba93c7696a6c466acd0ffb54419f25a24
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2022-02-25 17:01:44 +0300

    product details api done

--- Includes/TableFunctions/ProductDetails.php
+++ Includes/TableFunctions/ProductDetails.php
@@ -148,97 +148,91 @@ class ProductDetails
 
         $itemRecords = array();
 
-        $this->menu_id = htmlspecialchars(strip_tags($_GET["menuId"]));
+        $this->productId = htmlspecialchars(strip_tags($_GET["productID"]));
         $this->pageno = htmlspecialchars(strip_tags($_GET["page"]));
         $this->categoryID = htmlspecialchars(strip_tags($_GET["category"]));
 
-        if ($this->menu_id) {
+        if ($this->productId) {
             $this->pageno = floatval($this->pageno);
             $no_of_records_per_page = 6;
             $offset = ($this->pageno - 1) * $no_of_records_per_page;
 
-            $sql = "SELECT COUNT(*) as count FROM " . $this->itemsTable . " WHERE menu_type_id = " . $this->categoryID . " limit 1";
-            $result = mysqli_query($this->conns, $sql);
+            $sql = "SELECT COUNT(*) as count FROM products WHERE published = 1 AND id != " . $this->productId . " AND category_id = " . $this->categoryID . " limit 1";
+            $result = mysqli_query($this->conn, $sql);
             $data = mysqli_fetch_assoc($result);
             $total_rows = floatval($data['count']);
             $total_pages = ceil($total_rows / $no_of_records_per_page);
 
-
-            $menu_type_sql = "SELECT menu_id,menu_name, price, description, menu_type_id, menu_image,backgroundImage,ingredients, menu_status, created, modified,rating FROM " . $this->itemsTable . " WHERE menu_id = " . $this->menu_id . " limit 1";
-            $menu_type_result = mysqli_query($this->conns, $menu_type_sql);
-            $menu_type_data = mysqli_fetch_assoc($menu_type_result);
-
-            $menu_id = $menu_type_data['menu_id'];
-            $menu_name = $menu_type_data['menu_name'];
-            $price = $menu_type_data['price'];
-            $description = $menu_type_data['description'];
-            $menu_type_id = $menu_type_data['menu_type_id'];
-            $menu_image = $menu_type_data['menu_image'];
-            $backgroundImage = $menu_type_data['backgroundImage'];
-            $ingredients = $menu_type_data['ingredients'];
-            $menu_status = $menu_type_data['menu_status'];
-            $created = $menu_type_data['created'];
-            $modified = $menu_type_data['modified'];
-            $rating = $menu_type_data['rating'];
-
-
             $itemRecords["page"] = $this->pageno;
-            $itemRecords["results"] = array();
+            $itemRecords["selectedProduct"] = array();
             $itemRecords["total_pages"] = $total_pages;
             $itemRecords["total_results"] = $total_rows;
 
 
-            $stmt = $this->conns->prepare("SELECT menu_id,menu_name, price, description, menu_type_id, menu_image,backgroundImage,ingredients, menu_status, created, modified,rating FROM " . $this->itemsTable . " WHERE menu_id != " . $this->menu_id . " AND menu_type_id = " . $this->categoryID . " ORDER BY menu_id LIMIT " . $offset . "," . $no_of_records_per_page);
-        } else {
-            $stmt = $this->conns->prepare("SELECT menu_id,menu_name, price, description, menu_type_id, menu_image,backgroundImage, ingredients, menu_status, created, modified,rating FROM " . $this->itemsTable);
-        }
+            if ($this->pageno == 1) {
+
+                $product = new Product($this->conn, $this->productId);
+
+                if($product){
+                    $temp = array();
+                    $temp['id'] = $product->getId();
+                    $temp['name'] = $product->getName();
+                    $temp['category_id'] = $product->getCategory_id();
+                    $temp['photos'] = $product->getPhotos();
+                    $temp['thumbnail_img'] = $product->getThumbnail_img();
+                    $temp['unit_price'] = $product->getUnit_price();
+                    $temp['discount'] = $product->getDiscount();
+                    $temp['purchase_price'] = $product->getPurchase_price();
+                    $temp['meta_title'] = $product->getMeta_title();
+                    $temp['meta_description'] = $product->getMeta_description();
+                    $temp['meta_img'] = $product->getMeta_img();
+                    $temp['min_qtn'] = $product->getMin_qty();
+                    $temp['published'] = $product->getPublished();
 
 
-        $stmt->execute();
-        $stmt->bind_result($menu_id, $menu_name, $price, $description, $menu_type_id, $menu_image, $backgroundImage, $ingredients, $menu_status, $created, $modified, $rating);
+                    array_push($itemRecords["selectedProduct"], $temp);
+
+                }
+
+            }
 
 
-        if ($this->pageno == 1) {
-            $menudetailtemp = array();
+            // get products id from the same cat
+            $same_cat_IDs = array();
+            $same_cat_stm = "SELECT id FROM products  WHERE published = 1 AND id != " . $this->productId . " AND  category_id = " . $this->categoryID . " LIMIT " . $offset . "," . $no_of_records_per_page;
+            $same_cat_id_result = mysqli_query($this->conn, $same_cat_stm);
+            while ($row = mysqli_fetch_array($same_cat_id_result)) {
+                array_push($same_cat_IDs, $row['id']);
+            }
+
+            $allProducts = array();
+
+            foreach ($same_cat_IDs as $row) {
+                $product = new Product($this->conn,$row);
+                $temp = array();
+                $temp['id'] = $product->getId();
+                $temp['name'] = $product->getName();
+                $temp['category_id'] = $product->getCategory_id();
+                $temp['photos'] = $product->getPhotos();
+                $temp['thumbnail_img'] = $product->getThumbnail_img();
+                $temp['unit_price'] = $product->getUnit_price();
+                $temp['discount'] = $product->getDiscount();
+                $temp['purchase_price'] = $product->getPurchase_price();
+                $temp['meta_title'] = $product->getMeta_title();
+                $temp['meta_description'] = $product->getMeta_description();
+                $temp['meta_img'] = $product->getMeta_img();
+                $temp['min_qtn'] = $product->getMin_qty();
+                $temp['published'] = $product->getPublished();
+
+                array_push($allProducts, $temp);
+            }
+
+            $slider_temps = array();
+            $slider_temps['similarProducts'] = $allProducts;
+            array_push($itemRecords['selectedProduct'], $slider_temps);
 
 
-            $menudetailtemp['menu_id'] = floatval($menu_id);
-            $menudetailtemp['menu_name'] = $menu_name;
-            $menudetailtemp['price'] = floatval($price);
-            $menudetailtemp['description'] = $description;
-            $menudetailtemp['menu_type_id'] = floatval($menu_type_id);
-            $menudetailtemp['menu_image'] = $this->ImageBasepath . $menu_image;
-            $menudetailtemp['backgroundImage'] = $this->ImageBasepath . $backgroundImage;
-            $menudetailtemp['ingredients'] = $ingredients;
-            $menudetailtemp['menu_status'] = floatval($menu_status);
-            $menudetailtemp['created'] = $created;
-            $menudetailtemp['modified'] = $modified;
-            $menudetailtemp['rating'] = floatval($rating);
 
-
-            array_push($itemRecords["results"], $menudetailtemp);
-        }
-
-
-        while ($stmt->fetch()) {
-
-            $temp = array();
-
-            $temp['menu_id'] = $menu_id;
-            $temp['menu_name'] = $menu_name;
-            $temp['price'] = $price;
-            $temp['description'] = $description;
-            $temp['menu_type_id'] = $menu_type_id;
-            $temp['menu_image'] = $this->ImageBasepath . $menu_image;
-            $temp['backgroundImage'] = $this->ImageBasepath . $backgroundImage;
-            $temp['ingredients'] = $description;
-            $temp['menu_status'] = $menu_status;
-            $temp['created'] = $created;
-            $temp['modified'] = $modified;
-            $temp['rating'] = $rating;
-
-
-            array_push($itemRecords["results"], $temp);
         }
 
 
